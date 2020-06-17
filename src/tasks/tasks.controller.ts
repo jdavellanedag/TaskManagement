61acf6880321c14edad90bb98082a7dcf6be6e7f
jdavellanedag <juldave.gal@hotmail.com>
2020-06-16 20:15:48 -0500

    Task CRUD added

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService: TasksService){ }
+
+    @Get()
+    getTasks(@Query() filterDto: GetTaskFilterDto): Task[]{
+        if(Object.keys(filterDto).length){
+            return this.taskService.getTaskWithFilters(filterDto);
+        }
+        return this.taskService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+       return this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void{
+        this.taskService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus,
+    ) {
+        return this.taskService.updateTaskStatus(id, status);
+    }
+
+}
